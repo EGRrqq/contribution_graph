44cfe22936fcbfb954f4e6b34897acd81c8adb97
EGRrqq <egrrqqdev@gmail.com>
2024-03-11 08:42:09 -0400

    feat: imitate on hover prototype, row
    - feat(js): render date
    - feat(css): base styles for squares
    - feat(html): temp squares

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-import './style.css'
-console.log("bruh");
+import "./style.css";
+
+// date stuff
+const today = new Date();
+const substractDays = (n: number) =>
+  new Date(new Date().setDate(today.getDate() - n));
+const formatDate = (date: Date) =>
+  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+// graph stuff
+const graph = document.getElementById("graph") as HTMLElement;
+[...graph.children].forEach((s, i) => {
+  s.textContent = formatDate(substractDays(i));
+});
