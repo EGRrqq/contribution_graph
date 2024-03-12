e2cf29a53433f10bd049a706e19983a477240aba
EGRrqq <egrrqqdev@gmail.com>
2024-03-12 05:18:28 -0400

    feat(ts): days support for graph
    feat(ts): move date funcs into separate file

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,28 @@
 import "./style.css";
-
-// date stuff
-const today = new Date();
-const substractDays = (n: number) =>
-  new Date(new Date().setDate(today.getDate() - n));
-const formatDate = (date: Date) =>
-  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+import { generateGraph } from "./controllers/graphController";
 
 const graphEl = document.getElementById("graph");
 window.addEventListener("load", init, { once: true });
 
-// draw ui, should be in separate file
+// draw graph data, for visualization purposes
 function init() {
   if (!graphEl) return;
-  const graphArr = drawGraph(351, 1, 7);
+  const graphArr = generateGraph({
+    days: 30,
+    daysPerWeek: 7,
+    initDay: 1,
+  });
+  console.log(graphArr);
 
   graphArr.forEach((r) => {
     const row = document.createElement("div");
 
     r.forEach((n) => {
       const section = document.createElement("section");
-      section.textContent = n.toString();
+      section.textContent = Object.keys(n)[0];
       row.appendChild(section);
     });
 
     graphEl.appendChild(row);
   });
-}
-
-// graph logic without ui
-function drawGraph(MAX = 31, INIT = 1, daysPerWeek = 7): number[][] {
-  let max = MAX;
-  const weeks: number[][] = [];
-  const week: number[] = [];
-
-  if (INIT > daysPerWeek) return weeks;
-
-  while (max > 0) {
-    week.push(max);
-    max -= daysPerWeek;
-  }
-  weeks.push(week);
-
-  return weeks.concat(drawGraph(MAX - 1, INIT + 1));
 }
