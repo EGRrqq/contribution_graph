ed5cb44027b848962058d9076edab1638ae54a03
EGRrqq <egrrqqdev@gmail.com>
2024-03-13 08:23:56 -0400

    feat: redo from scratch
    feat(ts): weeks support

--- src/controllers/dateController.ts
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-
-export const subtractDays = (n: number) =>
-  new Date(new Date().setDate(today.getDate() - n));
-
-export const formatDate = (date: Date) =>
-  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,76 @@
-import "./style.css";
-import { generateGraph } from "./controllers/graphController";
+import './style.css'
 
-const graphEl = document.getElementById("graph");
+// date stuff
+declare global {
+  interface Date {
+    getWeek(): number;
+  }
+}
+
+Date.prototype.getWeek = function() {
+  const onejan = new Date(this.getFullYear(), 0, 1);
+  return Math.ceil((((this.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7);
+}
+
+const today = new Date();
+const subtractDays = (n: number) =>
+  new Date(new Date().setDate(today.getDate() - n));
+
+const formatDate = (date: Date) =>
+  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+
+const getLastDates = (days: number) => {
+  const dates = [];
+  while (days >= 0) {
+    dates.push(formatDate(subtractDays(days)));
+    days--;
+  }
+
+  return dates;
+}
+
+
+// graph stuff
+interface IGroups {
+  [key: string]: Date[];
+}
 window.addEventListener("load", init, { once: true });
 
-// draw graph data, for visualization purposes
+const dates = getLastDates(30);
+const groups: IGroups = dates.reduce((acc: IGroups, d) => {
+  const date = new Date(Date.parse(d));
+  const yearWeek = `${date.getFullYear()}-${date.getWeek()}`;
+
+  if (!acc[yearWeek]) {
+    acc[yearWeek] = [];
+  }
+
+  acc[yearWeek][date.getDay()] = date;
+
+  return acc;
+
+}, {});
+
 function init() {
+  const graphEl = document.getElementById("graph");
   if (!graphEl) return;
-  const graphArr = generateGraph({
-    days: 30,
-    daysPerWeek: 7,
-    initDay: 1,
-  });
-  console.log(graphArr);
 
-  graphArr.forEach((r) => {
-    const row = document.createElement("div");
+  Object.values(groups).forEach((c, ci) => {
+    const col = document.createElement("div");
 
-    r.forEach((n) => {
+    c.forEach((n, ri) => {
       const section = document.createElement("section");
-      section.textContent = Object.keys(n)[0];
-      row.appendChild(section);
+      section.textContent = n.getDate().toString();
+      section.style.gridArea = `${ri + 1}/${ci + 1}`
+
+      col.style.gridArea = `1/${ci + 1}`
+      col.appendChild(section);
     });
 
-    graphEl.appendChild(row);
+    graphEl.appendChild(col);
   });
 }
+
+console.log(dates);
+console.log(groups);
